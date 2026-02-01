db6be1a9b1e374b144f0122d73133b57348bfb24
Amber1362 <131332415+Amber1362@users.noreply.github.com>
2026-02-01 19:31:04 +0530

    Updated Project files

--- event.js
+++ event.js
@@ -9,7 +9,9 @@ form.addEventListener('submit', function(e) {
    let nameValue = inputName.value;
 
    let inputEmail = document.getElementById('email');
-   let emailValue = inputEmail.value;
+   let emailValue = inputEmail.value
+   let emailRegex = /^[A-Za-z\d]+(?:[.+-][a-zA-Z\d]+)*@[a-zA-Z\d]+\.[a-zA-Z0-9]{2,}$/;
+    
 
    let inputAge = document.getElementById('age');
    let ageValue = inputAge.value;
@@ -31,13 +33,16 @@ form.addEventListener('submit', function(e) {
       alert('Enter the name');
    } else if(!emailValue) {
       alert('Enter the email');
-   } else {
+   }else if(!emailRegex.test(emailValue.trim())) {
+      alert('Invalid Email ❌');
+   } else if(emailValue) {
+      alert('Successfully Submitted ✅ ')
       container.append(para);
       inputName.value = '';
       inputEmail.value = ''; 
       inputAge.value = '';
       count++;
-      totalCount.textContent = 'Total Entries: ' + count
+      totalCount.textContent = 'Total Entries: ' + count;
       para.append(deleteBtn);
       para.append(editBtn);
       para.append(textSpan);
@@ -54,15 +59,21 @@ container.addEventListener('click', function(e) {
     if(e.target.classList.contains('edit-btn')) {
      let span = e.target.parentElement.querySelector('span');
 
+     let editEmailRegex = /^[A-Za-z\d]+(?:[.+-][a-zA-Z\d]+)*@[a-zA-Z\d]+\.[a-zA-Z0-9]{2,}$/;
+
      let newName = prompt('Enter new name: ', span.textContent.split(', ')[0].split(': ')[1]);
      let newEmail = prompt('Enter new email: ', span.textContent.split(', ')[1].split(': ')[1]);
      let newAge = prompt('Enter new age: ', span.textContent.split(', ')[2].split(': ')[1])
 
+     if(!editEmailRegex.test(newEmail)) {
+         alert('Invalid Email Id');
+         return;
+      }
      if(newName && newEmail && newAge) {
       newName = newName.trim();
       newEmail = newEmail.trim();
       newAge = newAge.trim();
-      if(newName && newEmail) {
+      if(newName && newEmail && newAge) {
           span.textContent = 'Name: ' + newName + ', Email: ' + newEmail + ', Age: ' + newAge;
       }
      }
